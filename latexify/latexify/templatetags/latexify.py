152354da50823ceacd26352a7d09599da5d54cbc
Mustafa Abualsaud <ammsa@users.noreply.github.com>
2016-06-20 05:12:31 -0400

    Update scripts.html to get settings variables

--- latexify/latexify/templatetags/latexify.py
+++ latexify/latexify/templatetags/latexify.py
@@ -82,3 +82,22 @@ def latexify(text,
 
     return {'latex_type': latex_type,
             'latex_text': mark_safe(text)}
+
+
+@register.tag
+def value_from_settings(parser, token):
+    try:
+        # split_contents() knows not to split quoted strings.
+        tag_name, var = token.split_contents()
+    except ValueError:
+        raise template.TemplateSyntaxError, "%r tag requires a single argument" % token.contents.split()[0]
+    return ValueFromSettings(var)
+
+
+class ValueFromSettings(template.Node):
+    def __init__(self, var):
+        self.arg = template.Variable(var)
+
+    def render(self, context):
+        return getattr(latex_settings,
+                       re.sub(r'\W+', '', str(self.arg)))
